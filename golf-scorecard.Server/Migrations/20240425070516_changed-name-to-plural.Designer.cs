ea2cdfcfeac4665f938e5786d687cbaf20688900
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2024-04-26 09:47:58 +0200

    changed names to plural and added test controllers

--- golf-scorecard.Server/Migrations/20240425070516_changed-name-to-plural.Designer.cs
+++ golf-scorecard.Server/Migrations/20240425070516_changed-name-to-plural.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using golf_scorecard.Data;
 
@@ -10,9 +11,11 @@ using golf_scorecard.Data;
 namespace golf_scorecard.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240425070516_changed-name-to-plural")]
+    partial class changednametoplural
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ golf-scorecard.Server/Services/ITestService.cs
@@ -0,0 +1,9 @@
+﻿using golf_scorecard.Server.Models;
+
+namespace golf_scorecard.Server.Services
+{
+    public interface ITestService
+    {
+        Task<SlopeRating> GetDataByPublicIdAsync(int id);
+    }
+}
