42c5ef2d8325fa8b93f2f4f51610dc29760b1447
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2024-05-17 10:55:33 +0200

    Scorecard and scorescreen are now logical finished

--- golf-scorecard.client/src/components/Game.jsx
+++ golf-scorecard.client/src/components/Game.jsx
@@ -3,7 +3,10 @@ import axios from 'axios'; // Import Axios
 import Tee from "./SelectGameChildren/Tee";
 import Course from "./SelectGameChildren/Course";
 import Gender from './SelectGameChildren/Gender';
+import ScoreCard from './ScoreCard';
+import ScoreScreen from './ScoreScreen';
 import Button from 'react-bootstrap/Button';
+
 
 const apiHost = "https://localhost:7287/api/Game";
 
@@ -12,15 +15,18 @@ const Game = ({ course, gender, tee, handicap, extraStrokes }) => {
     // Define state variables to store the data
     const [holes, setHoles] = useState([]);
     const [strokes, setStrokes] = useState([])
+    const [gameFinished, setGameFinished] = useState(false);
+    const [score, setScore] = useState(0);
+    //const result = ["Hole In One", "Albatros", "Eagle", "Birdie", "Par", "Boggey"]
 
-    const [scores, setScores] = useState(new Array(holes.length).fill(''));
+    //const [scores, setScores] = useState(new Array(holes.length).fill(''));
 
     // Function to handle input change for a specific hole
-    const handleScoreChange = (index, event) => {
-        const newScores = [...scores];
-        newScores[index] = event.target.value; // Update the score for the corresponding hole
-        setScores(newScores);
-    }; 
+    //const handleScoreChange = (index, event) => {
+    //    const newScores = [...scores];
+    //    newScores[index] = event.target.value; // Update the score for the corresponding hole
+    //    setScores(newScores);
+    //}; 
 
     
     useEffect(() => {
@@ -45,39 +51,56 @@ const Game = ({ course, gender, tee, handicap, extraStrokes }) => {
         }
     };
 
+    const setFinishedGame = (data) => {
+        // Update parent state based on child event
+        setScore(data.score);
+        setGameFinished(data.bool); //true
+        console.log('course from home: ', data.score)
+    };
+
 
 
     return (
         <div>
-            <h1>Golf Scorecard</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Hole</th>
-                        <th>Index</th>
-                        <th>Par</th>
-                        <th>Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {holes.map((hole, index) => (
-                        <tr key={hole.id}>
-                            <td>{hole.number}</td>
-                            <td>{hole.holeIndex}</td>
-                            <td>{hole.par}</td>
-                            <td>
-                                <input
-                                    type="number"
-                                    value={scores[index]}
-                                    onChange={(event) => handleScoreChange(index, event)}
-                                    min="0"
-                                />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {!gameFinished ? (
+                <ScoreCard holes={holes} submitScoreToGame={setFinishedGame} />
+            ) : (
+                <>
+                        {gameFinished ? <ScoreScreen score={score} holes={holes} strokes={extraStrokes} /> : null}
+                </>
+            )}
+
         </div>
+        //<div>
+        //    <h1>Golf Scorecard</h1>
+        //    <table>
+        //        <thead>
+        //            <tr>
+        //                <th>Hole</th>
+        //                <th>Index</th>
+        //                <th>Par</th>
+        //                <th>Score</th>
+        //            </tr>
+        //        </thead>
+        //        <tbody>
+        //            {holes.map((hole, index) => (
+        //                <tr key={hole.id}>
+        //                    <td>{hole.number}</td>
+        //                    <td>{hole.holeIndex}</td>
+        //                    <td>{hole.par}</td>
+        //                    <td>
+        //                        <input
+        //                            type="number"
+        //                            value={scores[index]}
+        //                            onChange={(event) => handleScoreChange(index, event)}
+        //                            min="0"
+        //                        />
+        //                    </td>
+        //                </tr>
+        //            ))}
+        //        </tbody>
+        //    </table>
+        //</div>
     );
 };
 
--- golf-scorecard.client/src/components/ScoreScreen.jsx
+++ golf-scorecard.client/src/components/ScoreScreen.jsx
@@ -9,17 +9,80 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container'
+import Table from 'react-bootstrap/Table';
+/*import '../App.css';*/
 
 const apiHost = "https://localhost:7287/api/Game";
 
-const ScoreScreen = ({ onChildEvent }) => {
+const ScoreScreen = ({ score, holes, strokes }) => {
 
+    const [comparedToHandicap2, setComparedToHandicap2] = useState(0)
+    const [totalPar, setTotalPar] = useState(0);
+
+    const netto = score - strokes;
+    const comparedToHandicap = netto - totalPar;
+    console.log(comparedToHandicap)
+
+    const formattedComparedToHandicap = comparedToHandicap > 0 ? `+${comparedToHandicap}` : comparedToHandicap.toString();
+
+    console.log(formattedComparedToHandicap);
+
+    useEffect(() => {
+        // Calculate totalPar when the component mounts
+        const calculateTotalPar = () => {
+            let total = 0;
+            holes.forEach(hole => {
+                total += hole.par;
+            });
+            setTotalPar(total);
+            console.log(total); // Log totalPar
+        };
+
+        calculateTotalPar();
+    }, [holes]);
 
 
     return (
-        <div>
-            <p>ScoreScreen</p>
-        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th colSpan={2}>Results</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Brutto: </td>
+                    <td>{score}</td>
+                </tr>
+                <tr>
+                    <td>Netto: </td>
+                    <td>{netto}</td>
+                </tr>
+                <tr>
+                    <td>Hcp +/-: </td>
+                    <td>{formattedComparedToHandicap}</td>
+                </tr>
+            </tbody>
+        </Table>
+        //<div>
+        //    <h1>Result</h1>
+        //    <table>
+        //        <thead>
+        //            <tr>
+        //                <th>Brutto</th>
+        //                <th>Netto</th>
+        //                <th>Compared to handicap</th>
+        //            </tr>
+        //        </thead>
+        //        <tbody>
+        //            <tr>
+        //                <td>{score}</td>
+        //                <td>{score - strokes}</td>
+        //                <td>{formattedComparedToHandicap}</td>
+        //            </tr>
+        //        </tbody>
+        //    </table>
+        //</div>
     );
 };
 
